0a0ac4c81cb5911913505838fd7fa0134e1a76bc
FedeLupianez <lupianezfederico@gmail.com>
2025-06-11 20:49:09 -0300

    Hago los cambios para usar la mac address de la placa

--- Backend/Tablas.py
+++ Backend/Tablas.py
@@ -1,8 +1,10 @@
-from sqlmodel import DATE, TIMESTAMP, Field, SQLModel
+from datetime import date, time
+from sqlmodel import Field, SQLModel
+from typing import Optional
 
 
 class USUARIO(SQLModel, table=True):
-    id_usuario: int | None = Field(default=None, primary_key=True)
+    id_usuario: int = Field(default=None, primary_key=True)
     nombre: str
     sexo: str
     foto_perfil: str
@@ -10,21 +12,22 @@ class USUARIO(SQLModel, table=True):
 
 
 class GENERADOR(SQLModel, table=True):
-    id_generador: int | None = Field(default=None, primary_key=True)
-    id_usuario: int | None = Field(default=None, foreign_key="USUARIO.id_usuario")
+    id_generador: str = Field(primary_key=True)
+    macAddress: Optional[str] = Field(default=None, nullable=True)
+    id_usuario: int = Field(foreign_key="usuario.id_usuario")
     ciudad: str
     calle: str
     numero_vivienda: int
 
 
-class MEDICIO_POR_HORA(SQLModel, table=True):
-    id_medicion: int | None = Field(default=None, primary_key=True)
-    id_generador: int | None = Field(default=None, foreign_key="GENERADOR.id_generador")
+class MEDICION_POR_HORA(SQLModel, table=True):
+    id_medicion: int = Field(primary_key=True)
+    id_generador: str = Field(foreign_key="generador.id_generador")
     voltaje_generado: int
     consumo: int
     velocidad_viento: int
     direccion_viento: int
     humedad: int
     temperatura: int
-    fecha: str
-    hora: str
+    fecha: Optional[date] = Field(default=None, nullable=False)
+    hora: Optional[time] = Field(default=None, nullable=False)
