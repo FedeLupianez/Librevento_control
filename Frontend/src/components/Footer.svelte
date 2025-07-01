a75f3aa7d5bd87f7630fc7a4769846326d70d5c6
katerine <oliva.katerine2023@gmail.com>
2025-06-30 23:46:24 -0300

    puse un link a la font, cambie margenes, añadi la linea del header, cambie color del fondo, empece a hacer los desplegables. agregue el footer :)

---
+++ Frontend/src/components/Footer.svelte
@@ -0,0 +1,9 @@
+<footer class="bg-[#5b6950] text-white py-10 mt-10">
+  <div class="flex flex-col flex justify-center items-center space-y-0"> 
+    <p class="text-sm text-center text-gray-800">@instituto.manueldefalla</p>
+    <!-- Texto principal -->
+    <p class="text-[17px]">Proyecto desarrollado por alumnos del Instituto Manuel de Falla.</p>
+    <p class="text-sm text-center text-gray-800">Lupiañez Federico   Oliva Katerine   Pelliza Martina   Secco Eliana</p>
+    
+  </div>
+</footer>
--- Frontend/src/components/Header.svelte
+++ Frontend/src/components/Header.svelte
@@ -8,9 +8,9 @@
     
 </script>
 
-<div class="container justify-between flex flex-row items-center p-5 border-b-1 border-b-[#2f3e2f]">
+<div class="container justify-between flex flex-row items-center py-5 px-15 border-b-0 border-b-[#2f3e2f]">
     {#if user.sex == "hombre"}
-        <h1 class="items-center text-center">
+        <h1 class="items-center text-center mr-15">
             <span class="text-[#2f3e2f]">Bienvenido,</span> <span class="text-[#c2b280]">{user.name}</span>
         </h1>
     {:else}
@@ -19,15 +19,29 @@
         </h1>
     {/if}
 
-    <div class="flex gap-x-30 text-lg">
+    <div class="flex gap-x-20 text-lg">
         <button class="cursor-pointer flex flex-row items-center justify-center gap-1.5 font-bold">
             principal
             <Icon icon="bx:leaf" class="w-5 h-5"/>
         </button>
-        <button class="cursor-pointer flex flex-row items-center justify-center gap-1 font-bold">
-            estadísticas
-            <img src={flechita} alt="flechita" class="w-3.5 h-3.5 relative -top-0.4"  />
-        </button>
+        <div class="relative group"> <!-- se hace una familia uwu de los div adentro -->
+            <button class="cursor-pointer flex items-center font-bold text-[#2f3e2f] gap-1">
+                estadísticas
+                <img src={flechita} alt="flechita" class="w-3.5 h-3.5 relative -top-0.4"  />
+            </button>
+
+            <!-- pestaña gris -->
+            <div class="absolute -top-1 left-0 hidden group-hover:block bg-[#b9c3c8] rounded-md px-3 py-1 z-10">
+                <span class="font-bold text-[#2f3e2f]">estadísticas</span>
+            </div>
+
+            <!-- Menú desplegable -->
+            <div class="absolute left-0 mt-1 hidden group-hover:flex flex-col bg-[#7A9660] rounded-md shadow-md w-52 z-20">
+                <a href="/consumo-dia" class="px-4 py-2 text-white font-bold hover:bg-[#6b8755] rounded-t-md">Consumo por día</a>
+                <a href="/consumo-mes" class="px-4 py-2 text-white font-bold hover:bg-[#6b8755]">Consumo por mes</a>
+                <a href="/alertas" class="px-4 py-2 text-white font-bold hover:bg-[#6b8755] rounded-b-md">Alertas de consumo</a>
+            </div>
+        </div>
         <button class="cursor-pointer flex flex-row items-center justify-center gap-1 font-bold">
             editar
             <img src={flechita} alt="flechita" class="w-3.5 h-3.5 relative -top-0.4"  />
@@ -37,4 +51,7 @@
     <button class="cursor-pointer">
         <img src={user.imageUrl} alt="Foto de perfil" class="rounded-full h-10 w-10">
     </button>
+
 </div>
+
+<div class="w-228 h-0.5 bg-gray-500"></div>
