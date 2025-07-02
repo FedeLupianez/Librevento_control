12ed9057e7c8bf27005b8ebf09be0ec616c9fedd
FedeLupianez <lupianezfederico@gmail.com>
2025-07-02 15:48:28 -0300

    Agrego el manejo de sesiones del lado del servidor para manejar los login, además de que agrego corsy agrego la definición async de los endpoint

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,9 @@
 from sqlmodel import create_engine
 from dotenv import dotenv_values
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 import Tablas
 
 # Carpeta de Services, (funciones para interactuar con la base de datos)
@@ -22,12 +25,20 @@ class Rutas:
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(SessionMiddleware, secret_key=Config["SECRET_KEY"])
 
 # GENERADOR :
 #   Página :
 # endpoint para crear un generador
 @app.post(Rutas.Generador)
-def crear_generador(generador: Tablas.GENERADOR):
+async def crear_generador(generador: Tablas.GENERADOR):
     try:
         return GeneradorService.crear(engine, generador)
     except HTTPException as error:
@@ -37,7 +48,7 @@ def crear_generador(generador: Tablas.GENERADOR):
 
 # endpoint para obtener un generador por su id
 @app.get(Rutas.Generador)
-def obtener_generador(id_generador: int):
+async def obtener_generador(id_generador: int):
     try:
         return GeneradorService.obtener(engine, id_generador)
     except HTTPException as error:
@@ -47,7 +58,7 @@ def obtener_generador(id_generador: int):
 
 # endpoint para borrar un generador por su id
 @app.delete(Rutas.Generador)
-def borrar_generador(id_generador: int):
+async def borrar_generador(id_generador: int):
     try:
         return GeneradorService.borrar(engine, id_generador)
     except HTTPException as error:
@@ -57,7 +68,7 @@ def borrar_generador(id_generador: int):
 
 #   Hardware :
 @app.patch(Rutas.Generador + "/config_mac")
-def config_macAddress(id_usuario: int, macAddress: str):
+async def config_macAddress(id_usuario: int, macAddress: str):
     try:
         return GeneradorService.config_macAddress(engine, id_usuario, macAddress)
     except HTTPException as error:
@@ -66,56 +77,83 @@ def config_macAddress(id_usuario: int, macAddress: str):
 
 
 # Endpoints de usuario :
+
+@app.get(Rutas.Usuario + '/actual')
+async def usuario_actual(request: Request):
+    usuario = request.session.get("usuario")
+    if not usuario:
+        raise HTTPException(status_code=401, detail="Usuario no logueado")
+    print(usuario)
+    return usuario
+
 @app.post(Rutas.Usuario)
-def crear_usuario(usuario: Tablas.USUARIO):
+async def crear_usuario(usuario: Tablas.USUARIO, request: Request):
     try:
-        return UsuarioService.crear(engine, usuario)
+        nuevoUsuario = UsuarioService.crear(engine, usuario)
+        id_usuario: int = nuevoUsuario["id"]
+
+        tempUsuario = UsuarioService.obtener(engine, id_usuario)
+        request.session["usuario"] = {
+           "id_usuario": tempUsuario["id_usuario"],
+           "nombre": tempUsuario["nombre"],
+           "email_usuario": tempUsuario["email"]
+        }
+        return {"message": "Usuario creado", "usuario": request.session["usuario"]}
     except HTTPException as error:
         print(error)
-        return error
+        raise error
 
 
 @app.get(Rutas.Usuario)
-def obtener_usuario(id_usuario: int):
+async def obtener_usuario(id_usuario: int):
     try:
         return UsuarioService.obtener(engine, id_usuario)
     except HTTPException as error:
         print(error)
-        return error
+        raise error
 
 
-@app.get(Rutas.Usuario)
-def obtener_id(email_usuario: str):
+@app.get(Rutas.Usuario + '/id')
+async def obtener_id(email_usuario: str):
     try:
         return UsuarioService.obtener_id(engine, email_usuario)
     except HTTPException as error:
         print(error)
-        return error
+        raise error
 
 
 @app.delete(Rutas.Usuario)
-def borrar_usuario(id_usuario: int):
+async def borrar_usuario(id_usuario: int):
     try:
         return UsuarioService.borrar(engine, id_usuario)
     except HTTPException as error:
         print(error)
-        return error
+        raise error
 
+class loginInput(BaseModel):
+    email_usuario: str
+    clave: str
 
-@app.get(Rutas.Login)
-def login(email_usuario: str, clave: str):
+@app.post(Rutas.Login)
+async def login(data: loginInput, request: Request):
     try:
-        return UsuarioService.login(engine, email_usuario, clave)
+        usuario = UsuarioService.login(engine, data.email_usuario, data.clave)
+        request.session['usuario'] = {
+            "id_usuario": usuario["id_usuario"],
+            "nombre": usuario["nombre"],
+            "email_usuario": usuario["email"]
+        }
+        return {"message": "Usuario logueado", "usuario": request.session["usuario"]}
     except HTTPException as error:
         print(error)
-        return error
+        raise error
 
 
 # Endpoints para las mediciones:
 @app.post(Rutas.Medicion)
-def crear(medicion: Tablas.MEDICION_POR_HORA):
+async def crear(medicion: Tablas.MEDICION_POR_HORA):
     try:
         return MedicionService.crear(engine, medicion)
     except HTTPException as error:
         print(error)
-        return error
+        raise error
