38df9058b7aefaa02a5689ef014147c8e533c070
FedeLupianez <lupianezfederico@gmail.com>
2025-07-02 15:46:42 -0300

    Hago que el header cambie si está logueado o no, usando la store de usuario

--- Frontend/src/components/Header.svelte
+++ Frontend/src/components/Header.svelte
@@ -1,26 +1,30 @@
 <script lang="ts">
-	import type { User } from '../types/user';
 	import Icon from '@iconify/svelte';
 	import flechita from '/public/flechita.svg';
+	import { onMount } from 'svelte';
+	import { user, fetchUser } from '../stores/user';
 
-	export let user: User | null;
+	onMount(() => {
+		fetchUser();
+		console.log($user);
+	});
 </script>
 
 <div
 	class="container flex flex-row items-center justify-between border-b-0 border-b-[#2f3e2f] px-15 py-5"
 >
 	<h1 class="mr-15 items-center text-center">
-		{#if !user}
+		{#if !$user}
 			<span class="text-[#2f3e2f]">Bienvenido</span>
 		{:else}
 			<span class="text-[#2f3e2f]">
-				{#if user.sex == 'hombre'}
+				{#if $user.sexo == 'male'}
 					Bienvenido,
 				{:else}
 					Bienvenida,
 				{/if}
 			</span>
-			<span class="text-[#c2b280]">{user.name}</span>
+			<span class="text-[#c2b280]">{$user.nombre}</span>
 		{/if}
 	</h1>
 
@@ -87,7 +91,7 @@
 				<div
 					class="mt-1 hidden w-full min-w-52 flex-col rounded-md bg-[#7A9660] shadow-md group-hover:flex"
 				>
-					<a href="/login" class="px-4 py-2 font-bold hover:bg-[#6b8755]">
+					<a href="/login" class="rounded-t-md px-4 py-2 font-bold hover:bg-[#6b8755]">
 						<span class="text-white">Iniciar Sesión</span>
 					</a>
 					<a href="/sign_up" class="rounded-b-md px-4 py-2 font-bold text-white hover:bg-[#6b8755]">
@@ -99,8 +103,8 @@
 	</div>
 
 	<button class="cursor-pointer">
-		{#if user}
-			<img src={user.imageUrl} alt="Foto de perfil" class="h-10 w-10 rounded-full" />
+		{#if $user}
+			<img src={$user.foto_perfil} alt="Foto de perfil" class="h-10 w-10 rounded-full" />
 		{:else}
 			<Icon icon="bx:user" class="h-10 w-10 cursor-pointer rounded-full" />
 		{/if}
