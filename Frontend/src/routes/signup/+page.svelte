7771210a55f9281cfaeac6fe3604de8a15ad2550
FedeLupianez <lupianezfederico@gmail.com>
2025-07-03 10:37:40 -0300

    Cambio la página se sign up y agrego genderize al crear el usuario

--- Frontend/src/routes/signup/+page.svelte
+++ Frontend/src/routes/signup/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { fetchUser, user } from '../../stores/user';
+	import * as Genderize from 'genderize';
 
 	let name: string = '';
 	let email: string = '';
 	let password: string = '';
 
 	const handleSignUp = async () => {
-		const imageUrl: string = `https://ui-avatars.com/api/?name=${encodeURIComponent(name)}&size=128&background=random&color=fff&rounded=true&format=svg`;
+		const imageUrl: string = `https://ui-avatars.com/api/?name=${name.replace(' ', '+')}&size=128&background=random&color=fff&rounded=true&format=svg`;
+		const sexo: string = Genderize.getGender(name).gender;
 
 		const response = await fetch('http://localhost:8000/user', {
 			method: 'POST',
@@ -17,7 +19,7 @@
 				nombre: name,
 				email_usuario: email,
 				clave: password,
-				sexo: 'male',
+				sexo: sexo,
 				foto_perfil: imageUrl
 			}),
 			credentials: 'include'
@@ -35,45 +37,46 @@
 	<h1 class="text-5xl font-bold">Crear Cuenta</h1>
 
 	{#if !$user}
-		<!-- Acá se envía la info del usuario -->
-		<!-- <form action="/" method="POST" class="flex flex-col gap-5"> -->
-		<input
-			type="text"
-			name="name"
-			placeholder="Nombre de usuario"
-			class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
-			id="name-input"
-			bind:value={name}
-			required
-		/>
+		<div class="flex flex-col w-full min-h-150 items-center justify-center gap-4">
+			<input
+				type="text"
+				name="name"
+				placeholder="Nombre de usuario"
+				class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
+				id="name-input"
+				bind:value={name}
+				required
+			/>
 
-		<input
-			type="email"
-			name="email"
-			placeholder="Correo electrónico"
-			class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
-			bind:value={email}
-			required
-		/>
+			<input
+				type="email"
+				name="email"
+				placeholder="Correo electrónico"
+				class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
+				bind:value={email}
+				required
+			/>
 
-		<input
-			type="password"
-			name="clave"
-			placeholder="Contraseña"
-			class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
-			bind:value={password}
-			required
-		/>
-		<button
-			type="submit"
-			class="flex items-center justify-center rounded-2xl bg-gray-500 text-center"
-			id="signup-button"
-			on:click|preventDefault={handleSignUp}
-		>
-			Crear cuenta
-		</button>
-		<!-- </form> -->
+			<input
+				type="password"
+				name="clave"
+				placeholder="Contraseña"
+				class="flex w-full items-center justify-center rounded-2xl border border-gray-500 text-center"
+				bind:value={password}
+				required
+			/>
+			<button
+				type="submit"
+				class="flex items-center justify-center rounded-2xl bg-gray-500 text-center"
+				id="signup-button"
+				on:click|preventDefault={handleSignUp}
+			>
+				Crear cuenta
+			</button>
+		</div>
 	{:else}
-		<p>Ya estás registrado</p>
+		<div class="flex flex-col w-full min-h-150">
+			<p>Ya estás registrado</p>
+		</div>
 	{/if}
 </div>
