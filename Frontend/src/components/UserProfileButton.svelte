2e947b659b40b9193a5ddfe390aa90915b2e6a77
FedeLupianez <lupianezfederico@gmail.com>
2025-07-04 21:48:28 -0300

    Separo el boton de la cuenta en un componente aparte

---
+++ Frontend/src/components/UserProfileButton.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { user } from '../stores/user';
+	import Icon from '@iconify/svelte';
+
+	let showProfile: boolean = false;
+</script>
+
+<div class="group relative">
+	<button class="cursor-pointer" on:click={() => (showProfile = !showProfile)}>
+		{#if $user}
+			<img src={$user.foto_perfil} alt="Foto de perfil" class="h-10 w-10 rounded-full" />
+		{:else}
+			<Icon icon="bx:user" class="h-10 w-10 cursor-pointer rounded-full" />
+		{/if}
+	</button>
+
+	{#if showProfile}
+		<div
+			class="absolute -top-4.5 -right-4.5 mt-1 min-w-52 flex-col rounded-md bg-[#b9c3c8] p-0.5 shadow-md"
+		>
+			<div class="flex flex-row items-center justify-around">
+				{#if $user}
+					<span class="p-5">{$user.nombre}</span>
+				{:else}
+					<span class="p-5">Usuario</span>
+				{/if}
+				<button class="flex cursor-pointer flex-row" on:click={() => (showProfile = !showProfile)}>
+					{#if $user}
+						<img src={$user.foto_perfil} alt="Foto de perfil" class="h-10 w-10 rounded-full" />
+					{:else}
+						<Icon icon="bx:user" class="h-10 w-10 cursor-pointer rounded-full" />
+					{/if}
+				</button>
+			</div>
+
+			<div class="flex flex-col items-center justify-center">
+				{#if $user}
+					<a
+						href="/logout"
+						class="flex w-full items-center justify-center rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#6b8755]"
+					>
+						<Icon icon="bx:log-out" class="h-5 w-5" />
+						<span class="rounded-2xl text-center text-red-500">Cerrar Sesión</span>
+					</a>
+				{:else}
+					<a
+						href="/login"
+						class="flex w-full items-center justify-center rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#6b8755]"
+					>
+						<span class="rounded-2xl text-center text-white">Iniciar Sesión</span>
+					</a>
+				{/if}
+			</div>
+		</div>
+	{/if}
+</div>
