b7a3c7f3ec5605bd2faa1230cdfd778c02fa888a
FedeLupianez <lupianezfederico@gmail.com>
2025-08-01 18:15:55 -0300

    Arreglo diseño del menu del perfil, y simplifico el código para los dropdown

--- Frontend/src/components/DropDownButton.svelte
+++ Frontend/src/components/DropDownButton.svelte
@@ -8,58 +8,41 @@
 	}
 </script>
 
-{#if mode === 'button'}
-	<div class="relative">
-		<button
-			class="flex cursor-pointer items-center gap-1 font-bold text-[#2f3e2f]"
-			on:click={() => toggle()}
-		>
-			{text}
-			<img src="/public/images/flechita.svg" alt="flechita" class="-top-0.4 relative h-3.5 w-3.5" />
-		</button>
+<div class={`group relative ${view_menu ? 'z-30' : ''}`}>
+	<button
+		class="relative z-20 flex cursor-pointer items-center gap-1 font-bold text-[#2f3e2f]"
+		on:click={() => toggle()}
+		on:mouseenter={() => {
+			if (mode == 'hover') view_menu = true;
+		}}
+		on:mouseleave={() => {
+			if (mode == 'hover') view_menu = false;
+		}}
+	>
+		{text}
+		<img src="/public/images/flechita.svg" alt="flechita" class="-top-0.4 relative h-3.5 w-3.5" />
+	</button>
 
+	{#if mode == 'button'}
 		{#if view_menu}
 			<div class="absolute -top-1 -left-2.5 z-10 flex flex-col rounded-md bg-[#b9c3c8] px-3 py-1">
-				<button
-					class="flex cursor-pointer items-center gap-1 font-bold text-[#2f3e2f]"
-					on:click={() => toggle()}
-				>
-					{text}
-					<img
-						src="/public/images/flechita.svg"
-						alt="flechita"
-						class="-top-0.4 relative h-3.5 w-3.5"
-					/>
-				</button>
+				<div class="h-5 w-5"></div>
 				<slot name="menu"></slot>
 			</div>
 		{/if}
-	</div>
-{:else if mode === 'hover'}
-	<div class="group relative">
-		<button
-			class="flex cursor-pointer items-center gap-1 font-bold text-[#2f3e2f]"
-			on:click={() => toggle()}
-		>
-			{text}
-			<img src="/public/images/flechita.svg" alt="flechita" class="-top-0.4 relative h-3.5 w-3.5" />
-		</button>
-
+	{:else}
 		<div
+			aria-hidden="true"
 			class="absolute -top-1 -left-2.5 z-10 hidden flex-col rounded-md bg-[#b9c3c8] px-3 py-1 group-hover:flex"
+			on:mouseenter={() => {
+				view_menu = true;
+			}}
+			on:mouseleave={() => {
+				view_menu = false;
+			}}
 		>
-			<button
-				class="flex cursor-pointer items-center gap-1 font-bold text-[#2f3e2f]"
-				on:click={() => toggle()}
-			>
-				{text}
-				<img
-					src="/public/images/flechita.svg"
-					alt="flechita"
-					class="-top-0.4 relative h-3.5 w-3.5"
-				/>
-			</button>
+			<div class="h-5 w-5"></div>
 			<slot name="menu"></slot>
 		</div>
-	</div>
-{/if}
+	{/if}
+</div>
--- Frontend/src/components/UserProfileButton.svelte
+++ Frontend/src/components/UserProfileButton.svelte
@@ -5,10 +5,9 @@
 	let showProfile: boolean = false;
 </script>
 
-<div class="group relative">
-	<button class="cursor-pointer" on:click={() => (showProfile = !showProfile)}>
+<div class={`group relative ${showProfile ? 'z-30' : ''}`}>
+	<button class="relative z-20 cursor-pointer" on:click={() => (showProfile = !showProfile)}>
 		{#if $user}
-			{console.log(`Url de la foto de perfil : ${$user.foto_perfil}`)}
 			<img
 				src={$user.foto_perfil}
 				alt="Foto de perfil"
@@ -21,46 +20,47 @@
 
 	{#if showProfile}
 		<div
-			class="absolute -top-4.5 -right-4.5 mt-1 min-w-52 flex-col rounded-md bg-[#b9c3c8] p-0.5 shadow-md"
+			class="absolute -top-3.5 -right-3.5 z-10 mt-1 min-w-52 flex-col rounded-md bg-[#ddd5d5] p-4 shadow-md"
 		>
-			<div class="flex flex-row items-center justify-around">
+			<div class="flex flex-row items-center justify-around gap-5 border-b-1 border-black">
 				{#if $user}
-					<span class="p-5">{$user.nombre}</span>
+					{console.log($user)}
+					<div class="flex flex-col items-center justify-center gap-1">
+						<span class="text-center text-[#2f3e2f]">{$user.nombre}</span>
+						<span class="text[#2f3e2f] text-center text-[1rem]">{$user.email}</span>
+					</div>
 				{:else}
 					<span class="p-5">Usuario</span>
 				{/if}
-				<button class="flex cursor-pointer flex-row" on:click={() => (showProfile = !showProfile)}>
-					{#if $user}
-						<img
-							src={$user.foto_perfil}
-							alt="Foto de perfil"
-							class="h-15 min-h-15 w-15 min-w-15 rounded-full"
-						/>
-					{:else}
-						<Icon icon="bx:user" class="h-15 w-15 cursor-pointer rounded-full" />
-					{/if}
-				</button>
+				<div class="h-15 w-15"></div>
 			</div>
 
 			<div class="flex flex-col items-center justify-center">
 				{#if $user}
 					<button
-						class="flex w-full items-center justify-center rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#6b8755]"
+						class="flex w-full items-center justify-center gap-5 rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#b4b1b1]"
+					>
+						<Icon icon="pepicons-pencil:gear" class="h-5 w-5" />
+						<span class="rounded-2xl text-center">Configuración</span>
+					</button>
+
+					<button
+						class="flex w-full items-center justify-center gap-5 rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#b4b1b1]"
 						on:click={logoutUser}
 					>
 						<Icon icon="bx:log-out" class="h-5 w-5" />
-						<span class="rounded-2xl text-center text-red-500">Cerrar Sesión</span>
+						<span class="rounded-2xl text-center text-[#c85d4d]">Cerrar Sesión</span>
 					</button>
 				{:else}
 					<a
 						href="/signup"
-						class="flex w-full items-center justify-center rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#6b8755]"
+						class="flex w-full items-center justify-center gap-5 rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#b4b1b1]"
 					>
 						<span class="rounded-2xl text-center text-white">Crear Cuenta</span>
 					</a>
 					<a
 						href="/login"
-						class="flex w-full items-center justify-center rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#6b8755]"
+						class="flex w-full items-center justify-center gap-5 rounded-2xl px-4 py-2 font-bold text-white hover:bg-[#b4b1b1]"
 					>
 						<span class="rounded-2xl text-center text-white">Iniciar Sesión</span>
 					</a>
