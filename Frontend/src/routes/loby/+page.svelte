3a7e234057a1e2d3bf0e11251e8fa607fc7ce92e
FedeLupianez <lupianezfederico@gmail.com>
2025-08-06 16:58:19 -0300

    Agrego google fonts para usar sus fuentes

---
+++ Frontend/src/routes/loby/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+</script>
+
+<main class="min-h-screen gap-4 p-5">
+	<section class="flex flex-col gap-2">
+		<div class="flex flex-row gap-2">
+			<span class="text-2xl font-bold text-black">2.</span>
+			<span class="text-2xl font-bold text-black">Centro de control</span>
+		</div>
+		<p>
+			Como parte de las nuevas tecnologías integradas en nuestro aerogenerador, incorporamos una
+			interfaz que facilita la interacción con la energía generada. El Centro de Control de
+			Librevento permite visualizar, a través de gráficos, toda la información relevante sobre la
+			actividad del generador a lo largo del día.
+		</p>
+	</section>
+
+	<section class="flex flex-col">
+		<span class="text-2xl font-bold text-black"> Tutorial </span>
+		<div class="flex h-56 w-full bg-[#7A9660]"></div>
+		<p>
+			1. Pestaña “principal”: hace referencia a la sección que se visualiza en la imagen. En esta
+			página se puede encontrar: un gráfico con la información de los voltios generados en la
+			semana, un panel donde se señala el día con mayor voltios registrado y un gráfico de lo
+			consumido en el día de la energía generada.
+		</p>
+
+		<p>
+			2. Pestaña “estadisticas”: en la siguiente sección, se puede elegir entre las siguientes
+			opciones: Consumo por día Consumo por mes En estas dos opciones, el usuario puede elegir la
+			fecha exacta de la que quiere consultar los datos y se le mostrará un gráfico de los
+			movimientos estadìsticos guardados.
+		</p>
+
+		<p>
+			3. Pestaña “editar”: aca se encuentra la opción para cambiar el aspecto de las paginas al
+			“modo oscuro”. También, se puede dirigir al menú de configuración de WIFI.
+		</p>
+
+		<p>
+			4. Foto de perfil: si selecciona esta opción, el usuario encontrará: Editar datos de usuario.
+			Cerrar sesión
+		</p>
+
+		<p>
+			5. “Más información”: esta opción lo va a re-dirigir hacia un pequeño panel donde se podrá ver
+			que características atmosfericas presentó el día más eficiente.
+		</p>
+	</section>
+</main>
