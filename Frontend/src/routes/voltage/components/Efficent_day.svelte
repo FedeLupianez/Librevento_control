07004a0c15e7382cd3a33df3cd5baa0bc4e52939
FedeLupianez <lupianezfederico@gmail.com>
2025-08-13 16:37:39 -0300

    Agrego el diseño de diá más eficiente junto con los datos de minimo y máximo voltaje generado

---
+++ Frontend/src/routes/voltage/components/Efficent_day.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let date: string;
+	let day_name = new Date(date).toLocaleDateString('es-ES', { weekday: 'long' });
+</script>
+
+<div class="flex flex-row items-center justify-center gap-2">
+	<div class="flex flex-col items-center justify-start">
+		<p>El día mas eficiente de la semana fué el</p>
+		<span class="text-4xl font-bold text-[#7A9660]">{day_name}</span>
+	</div>
+	<img src="/public/images/hojas.svg" alt="Hojita" />
+</div>
