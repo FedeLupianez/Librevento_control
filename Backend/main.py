853cec4eb9c48dde099cd7004758054b7b15f475
FedeLupianez <lupianezfederico@gmail.com>
2025-09-17 17:55:47 -0300

    Agrego archivos necesarios para vercel para que lo pueda desplegar

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from dependencies import get_config
+from mangum import Mangum
 
 # Carpeta de Routers
 from Routers import UsuarioRouter, GeneradorRouter, MedicionRouter
@@ -10,6 +11,7 @@ from Routers import UsuarioRouter, GeneradorRouter, MedicionRouter
 Config: dict = get_config()
 
 app = FastAPI()
+handler = Mangum(app)
 
 app.add_middleware(
     CORSMiddleware,
